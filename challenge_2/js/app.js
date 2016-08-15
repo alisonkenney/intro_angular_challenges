92b88246ed6c056145b14f5930f1c8076ba28850
Alison Kenney <alisonkenney1@gmail.com>
2016-08-15 09:15:52 -0600

    done with all challenges

--- challenge_2/js/app.js
+++ challenge_2/js/app.js
@@ -1,8 +1,28 @@
 console.log('challenge #2 -- app.js loaded!');
 
-// var app = angular.module("appTwo", []);
-// app.controller("WelcomeController", WelcomeController);
+var app = angular.module("appTwo", []);
 
-// function WelcomeController(){
-//   this.full_name = "YOUR NAME"
-// }
+app.controller("WelcomeController", WelcomeController);
+app.controller("ClassController", ClassController);
+
+function WelcomeController(){
+	this.full_name = "Alison Kenney";
+	this.age = 26;
+	this.city = "Fort Collins";
+	this.state = "Colorado";
+	this.shout = function(name){
+	    return name.toUpperCase() + "!";
+	  };
+}
+
+function ClassController(){
+  this.class_name = "WDI";
+  this.enrolled_students = ["Alison", "Greg", "Maddy", "Charlie", "Matt", "Luis", "Monica", "Jamie", "Holly", "Kacey", "Mark", "Abe", "Nick"];
+  this.start_date = "6/13/2016";
+  this.end_date = "9/02/2016";
+  this.daysRemaining = function(){
+    var months_left = Date.parse(this.end_date) - Date.now();
+    var days_left = Math.floor( months_left/1000/24/60/60 );
+    return days_left;
+  };
+}
--- challenge_3/js/app.js
+++ challenge_3/js/app.js
@@ -1,9 +1,25 @@
 console.log('challenge #3 -- app.js loaded!');
 
-// var app = angular.module("appThree", []);
-// app.controller("infoController", infoController)
+var app = angular.module("appThree", []);
+app.controller("infoController", infoController);
+app.controller("loanController", loanController);
 
-// function infoController(){
-//   this.name = "Jon Doe"; // default name
-//   this.height = 5.6; // default height in feet
-// }
+function infoController(){
+	this.name = "Jon Doe"; // default name
+	this.height = 5.6; // default height in feet
+}
+
+function loanController(){
+  this.amount = 1000;
+  this.apr = 3.5;
+  this.month = function(x){
+    var yearly_interest = (this.amount * (this.apr / 100) );
+    var monthly_interest = ( yearly_interest / 12 );
+    var accrued_interest = x * monthly_interest || 0;
+    return {
+      accrued_interest: accrued_interest,
+      total_balance: this.amount + accrued_interest
+    };
+  };
+}  
+
